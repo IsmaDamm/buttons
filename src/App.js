5a174a0c63fdc012922e9178296128dda18f3ced
ismaDamm <issmaelhassouni@gmail.com>
2022-06-17 00:12:01 +0200

    trying

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ import {Button} from "./components/Button"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Button >Default</Button>
-      <Button variant={"outline"}>Default</Button>
-      <Button variant={"text"}>Default</Button>
-      <Button shadow={"disableShadow"}>Default</Button>
-      <Button disabled={"disable"}>Default</Button>
-      <Button variant="text" disabled={"disable"}>Default</Button>
-      <Button color={"default"}>Default</Button>
-      <Button color={"primary"}>Default</Button>
-      <Button color={"secondary"}>Default</Button>
-      <Button color={"danger"}>Default</Button>
-      <Button size={"sm"}>Default</Button>
-      <Button size={"md"}>Default</Button>
-      <Button size={"lg"}>Default</Button>
-      </header>
+        <h1>Buttons</h1>
+        <div className='container'></div>
+          <div className='buttons'>
+          <p>{"<Button />"}</p>
+            <Button >Default</Button>
+            <p>{"<Hover & Focus />"}</p>
+            <Button variant={"outline"}>Default</Button>
+          </div>
+          <Button variant={"text"}>Default</Button>
+          <Button shadow={"disableShadow"}>Default</Button>
+          <Button disabled={"disable"}>Default</Button>
+          <Button variant="text" disabled={"disable"}>Default</Button>
+          <Button color={"default"}>Default</Button>
+          <Button color={"primary"}>Default</Button>
+          <Button color={"secondary"}>Default</Button>
+          <Button color={"danger"}>Default</Button>
+          <Button size={"sm"}>Default</Button>
+          <Button size={"md"}>Default</Button>
+          <Button size={"lg"}>Default</Button>
     </div>
   );
 }
